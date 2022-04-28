30e041627c87ae9e877ebf889013a140c2e496a7
lncainwa <lara.cain@yahoo.com>
2022-04-27 20:25:30 -0700

    Add files via upload
    first commit for homework challenge

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+var startButton = document.getElementById('start-btn')
+var nextButton = document.getElementById('next-btn')
+
+var questionContainerElement = document.getElementById('question-container')
+var quesitonElement = document.getElementById('quesiton')
+var answerButtonsElement = document.getElementById('answer-buttons')
+
+let 
